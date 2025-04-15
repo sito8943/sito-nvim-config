7f42314c324e270d24bf54d7d0d8d7cbd4abcaa5
Sito <sito8943@gmail.com>
2025-04-15 10:35:29 -0400

    toggle term created

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,7 @@
+require("toggleterm").setup{
+  open_mapping = [[<C-M-t>]], -- Ctrl + alt ((M)eta key) + t to toggle
+  direction = 'vertical', -- can also be 'float' or 'horizontal'
+  start_in_insert = true,
+  size = 50,
+}
+
