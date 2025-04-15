20199998ee5fdf2e1579d4204fe59d62273c0c03
Sito <sito8943@gmail.com>
2025-04-15 09:37:27 -0400

    set and lsp

--- lua/sito8943/packer.lua
+++ lua/sito8943/packer.lua
@@ -30,7 +30,25 @@ return require('packer').startup(function(use)
 	    'nvim-tree/nvim-web-devicons', -- optional
 	  },
 	}
-	use({'neovim/nvim-lspconfig'})
-	use({'hrsh7th/nvim-cmp'})
-	use({'hrsh7th/cmp-nvim-lsp'})
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		requires = {
+			--LSP Suppor
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	}
 end)
