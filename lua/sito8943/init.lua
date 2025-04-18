cbabd63e2fe8878401ac7937b6f5cc82051e68c7
Sito8943 <sito8943@gmail.com>
2025-04-17 22:09:24 -0400

    auto save

--- lua/sito8943/init.lua
+++ lua/sito8943/init.lua
@@ -1,7 +1,7 @@
 require("sito8943.remap")
 require("sito8943.set")
 
---vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
---    pattern = "*",
---    command = "silent ! write",
---})
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+    pattern = "*",
+    command = "silent ! write",
+})
